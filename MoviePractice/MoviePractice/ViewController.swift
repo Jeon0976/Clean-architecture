6e6b3a51a1429b6b14da3f9e91f01a7eb7eb76c7
Jeon Seoung Hun <jsh097610@gmail.com>
2023-08-29 11:54:17 +0900

    [init]

---
+++ MoviePractice/MoviePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviePractice
+//
+//  Created by 전성훈 on 2023/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
